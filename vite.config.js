d3cd2d3990a3c75c5997d2b4ac9c068f3722a71c
Maks Chuprin <maksim_chuprin@MacBook-Pro-Maksim.local>
2025-10-03 15:34:04 +0300

    Updated tsconfig a little

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from "rollup-plugin-visualizer";
 import path from 'path'; 
 
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    outDir: "build",
+  },
+  plugins: [
+    react()
+    // visualizer({
+    // TODO: разобраться с visualizer
+    // }) // должен быть последним
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
